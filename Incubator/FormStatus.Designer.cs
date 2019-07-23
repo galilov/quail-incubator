fe7dca4bbfbb20f90bd3f3beab082288ade7990c
Alexander Galilov <alexander.galilov@gmail.com>
2019-07-23 10:55:27 +0400

    Windows app

---
+++ Incubator/FormStatus.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace Incubator
+{
+    partial class FormStatus
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lbTemperature = new System.Windows.Forms.Label();
+            this.lbHumidity = new System.Windows.Forms.Label();
+            this.lbHeating = new System.Windows.Forms.Label();
+            this.lbCover = new System.Windows.Forms.Label();
+            this.lbTray = new System.Windows.Forms.Label();
+            this.lbHours = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(12, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(170, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Temperature, C:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(12, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(136, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Humidity, %:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(12, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(175, 29);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Heating (PID), %:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(12, 127);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(183, 29);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Humidifier cover:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(12, 156);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(146, 29);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Tray position:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.ForeColor = System.Drawing.Color.Black;
+            this.label6.Location = new System.Drawing.Point(12, 185);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(213, 29);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Hours of incubation:";
+            // 
+            // lbTemperature
+            // 
+            this.lbTemperature.AutoSize = true;
+            this.lbTemperature.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbTemperature.ForeColor = System.Drawing.Color.Black;
+            this.lbTemperature.Location = new System.Drawing.Point(228, 40);
+            this.lbTemperature.Name = "lbTemperature";
+            this.lbTemperature.Size = new System.Drawing.Size(159, 29);
+            this.lbTemperature.TabIndex = 6;
+            this.lbTemperature.Text = "lbTemperature";
+            // 
+            // lbHumidity
+            // 
+            this.lbHumidity.AutoSize = true;
+            this.lbHumidity.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbHumidity.ForeColor = System.Drawing.Color.Black;
+            this.lbHumidity.Location = new System.Drawing.Point(228, 69);
+            this.lbHumidity.Name = "lbHumidity";
+            this.lbHumidity.Size = new System.Drawing.Size(123, 29);
+            this.lbHumidity.TabIndex = 7;
+            this.lbHumidity.Text = "lbHumidity";
+            // 
+            // lbHeating
+            // 
+            this.lbHeating.AutoSize = true;
+            this.lbHeating.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbHeating.ForeColor = System.Drawing.Color.Black;
+            this.lbHeating.Location = new System.Drawing.Point(225, 98);
+            this.lbHeating.Name = "lbHeating";
+            this.lbHeating.Size = new System.Drawing.Size(108, 29);
+            this.lbHeating.TabIndex = 8;
+            this.lbHeating.Text = "lbHeating";
+            // 
+            // lbCover
+            // 
+            this.lbCover.AutoSize = true;
+            this.lbCover.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbCover.ForeColor = System.Drawing.Color.Black;
+            this.lbCover.Location = new System.Drawing.Point(225, 127);
+            this.lbCover.Name = "lbCover";
+            this.lbCover.Size = new System.Drawing.Size(89, 29);
+            this.lbCover.TabIndex = 9;
+            this.lbCover.Text = "lbCover";
+            // 
+            // lbTray
+            // 
+            this.lbTray.AutoSize = true;
+            this.lbTray.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbTray.ForeColor = System.Drawing.Color.Black;
+            this.lbTray.Location = new System.Drawing.Point(226, 156);
+            this.lbTray.Name = "lbTray";
+            this.lbTray.Size = new System.Drawing.Size(74, 29);
+            this.lbTray.TabIndex = 10;
+            this.lbTray.Text = "lbTray";
+            // 
+            // lbHours
+            // 
+            this.lbHours.AutoSize = true;
+            this.lbHours.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbHours.ForeColor = System.Drawing.Color.Black;
+            this.lbHours.Location = new System.Drawing.Point(225, 185);
+            this.lbHours.Name = "lbHours";
+            this.lbHours.Size = new System.Drawing.Size(90, 29);
+            this.lbHours.TabIndex = 11;
+            this.lbHours.Text = "lbHours";
+            // 
+            // FormStatus
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(450, 265);
+            this.ControlBox = false;
+            this.Controls.Add(this.lbHours);
+            this.Controls.Add(this.lbTray);
+            this.Controls.Add(this.lbCover);
+            this.Controls.Add(this.lbHeating);
+            this.Controls.Add(this.lbHumidity);
+            this.Controls.Add(this.lbTemperature);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.HelpButton = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormStatus";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "FormStatus";
+            this.Load += new System.EventHandler(this.FormStatus_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lbTemperature;
+        private System.Windows.Forms.Label lbHumidity;
+        private System.Windows.Forms.Label lbHeating;
+        private System.Windows.Forms.Label lbCover;
+        private System.Windows.Forms.Label lbTray;
+        private System.Windows.Forms.Label lbHours;
+    }
+}
---
+++ Incubator/Tlv.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.IO;
+
+namespace Incubator
+{
+    class Tlv
+    {
+        private enum TlvState { ReadType, ReadLength, ReadData };
+        private TlvState _step = TlvState.ReadType;
+        private int _n, _offset;
+
+        public const int MaxDataLen = 32;
+        public TlvType Type { get; set; }
+        private int _len;
+
+        public int Len
+        {
+            get => _len;
+            set => _len = value > MaxDataLen ? MaxDataLen : value;
+        }
+
+        public readonly byte[] ValueBytes = new byte[MaxDataLen];
+
+        public void Reset()
+        {
+            Type = TlvType.Reset;
+            _n = 0;
+            _offset = 0;
+            _step = TlvState.ReadType;
+            _len = 0;
+        }
+
+        public void SetInt16Value(short val)
+        {
+            ValueBytes[_offset++] = (byte)(val & 0x0ff);
+            ValueBytes[_offset++] = (byte)((val >> 8) & 0x0ff);
+            Len += 2;
+        }
+
+        public void SetInt8Value(byte val)
+        {
+            ValueBytes[_offset++] = val;
+            Len += 1;
+        }
+
+        public short GetInt16Value()
+        {
+            short result = ValueBytes[_offset++];
+            result |= (short)(ValueBytes[_offset++] << 8);
+            return result;
+        }
+        public int GetInt32Value()
+        {
+            int result = ValueBytes[_offset++];
+            result |= ValueBytes[_offset++] << 8;
+            result |= ValueBytes[_offset++] << 16;
+            result |= ValueBytes[_offset++] << 24;
+            return result;
+        }
+
+        public byte GetInt8Value()
+        {
+            return ValueBytes[_offset++];
+        }
+
+        public void Write(Stream to)
+        {
+            to.WriteByte((byte)Type);
+            to.WriteByte((byte)Len);
+            to.Write(ValueBytes, 0, Len);
+        }
+
+        public bool ReadByte(int b)
+        {
+            Console.WriteLine(b);
+
+            if (b == -1) return false;
+            switch (_step)
+            {
+                case TlvState.ReadType:
+                    Type = (TlvType)b;
+                    if (Type == TlvType.Reset)
+                    {
+                        return true;
+                    }
+
+                    _step = TlvState.ReadLength;
+                    break;
+                case TlvState.ReadLength:
+                    Len = b;
+                    _n = b;
+                    if (Len == 0)
+                    {
+                        _step = TlvState.ReadType;
+                        return true;
+                    }
+                    _step = TlvState.ReadData;
+                    break;
+                case TlvState.ReadData:
+                    var idx = Len - _n--;
+                    ValueBytes[idx] = (byte)b;
+                    if (_n == 0)
+                    {
+                        _step = TlvState.ReadType;
+                        _offset = 0;
+                        return true;
+                    }
+
+                    _step = TlvState.ReadData;
+                    break;
+            }
+
+            return false;
+        }
+    }
+}
